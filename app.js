3ceedfeb8e10bb587f55cf09df8e778828ec37d3
quangvinh1102 <vinhls2002@gmail.com>
2022-08-12 21:23:37 +0700

    up

---
+++ app.js
@@ -0,0 +1,33 @@
+fetch('https://fakestoreapi.com/products')
+.then(res=>{
+    return res.json()
+})
+.then(data=>{
+    var products = document.querySelector('.products')
+    products.innerHTML = ''
+    data.forEach(item =>{
+        var newProduct =  document.createElement('div')
+        newProduct.classList.add('product')
+        newProduct.innerHTML = `
+            <img src="${item.image}">
+            <div class="info">
+                <div class="name">${item.title}</div>
+                <div class="price">$ ${item.price}</div>
+            </div>
+        `
+        products.appendChild(newProduct)
+})
+})
+
+var inputSearch = document.querySelector('.search input')
+inputSearch.addEventListener('input', function(e){
+    let txtSearch = e.target.value.trim().toLowerCase()
+    let listProductDOM = document.querySelectorAll('.product')
+    listProductDOM.forEach(item=>{
+        if(item.innerText.toLowerCase().includes(txtSearch)){
+            item.classList.remove('hide')
+        }else{
+            item.classList.add('hide')
+        }
+    })
+})
